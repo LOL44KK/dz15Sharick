4544498f6a7195973541388577d6ef9abb36e359
nikit <nikit@MYKYTATHINKBOOK>
2024-09-22 11:41:04 +0300

    Добавьте файлы проекта.

---
+++ dz15Sharick/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace dz15Sharick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+        }
+    }
+
+    public class Game
+    {
+        private delegate void Scene();
+        
+        private List<Scene> scenes;
+
+        public Game()
+        {
+            scenes = new List<Scene>();
+            scenes.Add(Exit);
+            scenes.Add(NewGame);
+            scenes.Add(LoadGame);
+            scenes.Add(Rules);
+            scenes.Add(Author);
+        }
+
+        public void Start()
+        {
+            Menu();
+        }
+
+        private void Menu()
+        {
+            while (true)
+            {
+                Console.WriteLine("MENU");
+                Console.WriteLine("1. Новая игра");
+                Console.WriteLine("2. Загрузить игру");
+                Console.WriteLine("3. Правила");
+                Console.WriteLine("4. Об авторе");
+                Console.WriteLine("0. Выход");
+
+                Console.Write(">");
+                string? fed = Console.ReadLine();
+                Console.Clear();
+
+                if (fed.All(char.IsDigit))
+                {
+                    scenes[int.Parse(fed)]();
+                }
+            }
+        }
+
+        private void NewGame()
+        {
+            Console.WriteLine("New Game");
+        }
+
+        private void LoadGame()
+        {
+            Console.WriteLine("Load Game");
+        }
+
+        private void Rules()
+        {
+            Console.WriteLine("Rules");
+        }
+
+        private void Author()
+        {
+            Console.WriteLine("Author");
+        }
+
+        private void Exit()
+        {
+            Console.WriteLine("Poka");
+            Environment.Exit(0);
+        }
+    }
+}
